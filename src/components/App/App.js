83742ba941300668ca27b50af42b0c1921401a49
Chengqi Zhu <chengqizhufm@gmail.com>
2019-08-18 14:40:36 -0700

    Initial app setup

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../images/logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      <h1>Chengqi's photography portfolio.</h1>
+    </div>
+  );
+}
+
+export default App;
