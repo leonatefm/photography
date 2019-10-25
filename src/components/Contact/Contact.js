c00922de25be5dec667487561deb4d5e34769d13
Chengqi Zhu <chengqizhufm@gmail.com>
2019-10-24 22:55:42 -0700

    Add Google Analytics tracking

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,26 +1,59 @@
 import './Contact.scss';
 import * as React from 'react';
 import icons from '../../lib/icons';
+import ReactGA from 'react-ga';
 
 class Contact extends React.PureComponent {
+  _handleContactClick = event => {
+    const contactMethod = event.currentTarget.dataset.method;
+    ReactGA.event({
+      category: 'Contact',
+      action: 'Click',
+      label: contactMethod
+    });
+  };
+
   render() {
     return (
       <div className="Contact">
-        <a className="Contact-link" href="https://www.weibo.com/leonate" target="_blank" rel="noopener noreferrer">
+        <a
+          className="Contact-link"
+          data-method="Weibo"
+          href="https://www.weibo.com/leonate"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={this._handleContactClick}
+        >
           {icons.weibo}
         </a>
         <a
           className="Contact-link"
+          data-method="Instagram"
           href="https://www.instagram.com/aprilopus/"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={this._handleContactClick}
         >
           {icons.instagram}
         </a>
-        <a className="Contact-link" href="https://twitter.com/aprilopus" target="_blank" rel="noopener noreferrer">
+        <a
+          className="Contact-link"
+          data-method="Twitter"
+          href="https://twitter.com/aprilopus"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={this._handleContactClick}
+        >
           {icons.twitter}
         </a>
-        <a className="Contact-link" href="mailto:chengqizhufm@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a
+          className="Contact-link"
+          data-method="Email"
+          href="mailto:chengqizhufm@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={this._handleContactClick}
+        >
           {icons.email}
         </a>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import './index.scss';
 import * as React from 'react';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Google Analytics
+ReactGA.initialize('UA-75265785-2');
+ReactGA.pageview('/');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
