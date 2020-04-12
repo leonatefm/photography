1d7d7b7e9b327f37fc8199890821cbb94730c286
Chengqi Zhu <chengqizhufm@gmail.com>
2020-04-12 15:09:11 -0700

    Add dark color scheme support

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,45 @@ import Gallery from '../Gallery';
 import Logo from '../Logo';
 import Nav from '../Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
-        <Nav />
-      </header>
-      <Gallery />
-      <footer className="App-footer">
-        <p>Designed by Chengqi Zhu &copy; 2019</p>
-        <Logo size="small" withName={false} />
-      </footer>
-    </div>
-  );
+class App extends React.PureComponent {
+  colorSchemeQuery = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+
+  componentDidMount() {
+    this.setColorTheme();
+    this.colorSchemeQuery && this.colorSchemeQuery.addListener(this._handleColorSchemeChange);
+  }
+
+  componentWillUnmount() {
+    this.colorSchemeQuery && this.colorSchemeQuery.removeListener(this._handleColorSchemeChange);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Logo />
+          <Nav />
+        </header>
+        <Gallery />
+        <footer className="App-footer">
+          <p>Designed by Chengqi Zhu &copy; 2019</p>
+          <Logo size="small" withName={false} />
+        </footer>
+      </div>
+    );
+  }
+
+  _handleColorSchemeChange = () => {
+    this.setColorTheme();
+  };
+
+  setColorTheme() {
+    if (this.colorSchemeQuery && this.colorSchemeQuery.matches) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
 }
 
 export default App;
